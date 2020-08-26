0b84285751132182effc1f58a27b3ca7e5dc297a
Xusk <Xusk947@gmail.com>
2020-08-26 22:48:26 +0500

    fast fix

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,7 @@
 //supporter
 require("content/units/supporter/t1")
 require("content/units/supporter/t2")
+require("content/units/supporter/t3")
 //battle-cruiser
 require("content/units/battle-cruiser/t1")
 
