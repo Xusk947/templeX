c544167b826d0c0a1329775bfaac99f859edc12c
Xusk947 <56998647+Xusk947@users.noreply.github.com>
2020-08-29 04:34:57 +0500

    Update t1.js

--- scripts/content/units/submarine/t1.js
+++ scripts/content/units/submarine/t1.js
@@ -6,26 +6,7 @@ const vindicate = extendContent(UnitType, "vindicate", {
 });
 
 vindicate.constructor = () => {
-	const unit = extend(CommanderUnitWaterMove, {
-		draw() {
-			if (unit.deep <= 1){
-				Draw.alpha(unit.deep)
-			}
-	        Draw.mixcol(unit.floorOn().mapColor, 1 - unit.deep);
-	        Draw.rect(vindicate.region, this.x, this.y, this.rotation - 90)
-			if (!unit.mounts[0].shoot) {
-				if (unit.deep >= 0.1) {
-					unit.deep = unit.deep - 0.02
-				}
-        	} else {
-        		if (unit.deep <= 1) {
-        			unit.deep = unit.deep + 0.03
-					
-				}
-	        Draw.reset()
-			}
-		}	
-	})
+	const unit = extend(CommanderUnitWaterMove, {})
 
 	unit.deep = 0
 	return unit
